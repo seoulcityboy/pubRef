c8f62cc6c7101e81320c01e475d26534624c101f
seoulcityboy <seoulcityboy@gmail.com>
2016-08-25 16:02:35 +0900

    탭 json

--- jsonData/js/dataJson.js
+++ jsonData/js/dataJson.js
@@ -1,22 +1,31 @@
 $(function () {
-    $.ajax({
-        type : "GET",
-        url : "../js/menu.json",
-        dataType : "json",
-        success : function(json) {
-           var outlaw = json.menu01;
-
-           console.log(json.usrName);
-            $(".dataView .objLst").each(function () {
-                var idx = $(this).index();
-                $(this).find(".name").text(outlaw[idx].usrName);
-                $(this).find(".job").text(outlaw[idx].usrJob);
-                $(this).find(".nickName").text(outlaw[idx].usrNickName);
-                $(this).find(".email").text(outlaw[idx].usrEmail);
-            });
-        },
-        error : function (e) {
-            alert("에러")
-        }
+    $(".tabName li").click(function(){
+        var typeName= $(this).index();
+        dataPush(typeName);
     });
+    function  dataPush(tabName) {
+        var dtName= tabName+1,
+            meunNum = "menu0",
+            totalVal = meunNum + dtName;
+        $.ajax({
+            type : "GET",
+            url : "../js/menu.json",
+            dataType : "json",
+            success : function(json) {
+                var outlaw = json,
+                    outlaw = outlaw[totalVal];
+                $(".dataView .objLst").each(function () {
+                    var idx = $(this).index();
+                    $(this).find(".name").text(outlaw[idx].usrName);
+                    $(this).find(".job").text(outlaw[idx].usrJob);
+                    $(this).find(".nickName").text(outlaw[idx].usrNickName);
+                    $(this).find(".email").text(outlaw[idx].usrEmail);
+                });
+            },
+            error : function (e) {
+                alert("에러")
+            }
+        });
+    }
+    dataPush(0);
 })
