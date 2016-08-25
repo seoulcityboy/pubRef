b9c4eeec8817945b8ca688c8c5fb5e3f0366dfcb
seoulcityboy <seoulcityboy@gmail.com>
2016-08-25 18:09:14 +0900

    실패중...

--- checkBox/js/checkBox.js
+++ checkBox/js/checkBox.js
@@ -1,3 +1,40 @@
 $(function(){
-    
+    var start = $(function () {
+        var $jschk= $(".jsChk"),
+            $inputChk= $jschk.find("input");
+        function chkClick(){
+            $inputChk.off("change").on("change",function () {
+                if($(this).prop("checked")===true){
+                    $(this).prop("checked", true).closest($jschk).addClass('active');
+                }else{
+                    $(this).prop("checked", false).closest($jschk).removeClass('active');
+                }
+            });
+        };
+        function chkTotal(name){
+            var name = name;
+            var checkCategory = $('input[name='+ name +']:checkbox'),
+                checkCategorychk = $('input[name='+ name + ']:checkbox:checked');
+
+
+            if($(this).is(":checked")==false){
+                $(checkCategory).each(function () {
+                    $(this).prop('checked',false);
+                    $(this).closest('.checkbox').removeClass('active');
+                });
+            } else if($(this).is(":checked")==true){
+                $(checkCategory).each(function () {
+                    $(this).prop('checked',true);
+                    $(this).closest('.checkbox').addClass('active');
+                });
+            }
+        }
+        chkClick();
+        $(".totalChk").on("click",function () {
+            var chkName =$(this).attr("name");
+            chkTotal(chkName);
+            console.log(chkName);
+        });
+
+    });
 });
