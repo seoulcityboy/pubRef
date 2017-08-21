b434eae663495b605ecfc563ff99a257105b0158
seoulcityboy <seoulcityboy@gmail.com>
2017-08-21 20:38:33 +0900

    미완

--- mobileTouchEvent/js/mobileTE.js
+++ mobileTouchEvent/js/mobileTE.js
@@ -1,1 +1,27 @@
 console.log("hello");
+var $thisWrap = $("#wrap"),
+	$thisBox = $(".moveBox"),
+	first_point = 0,
+	move_point = 0;
+
+$thisWrap.on('touchstart',tStart);
+$thisWrap.on('touchmove',tMove);
+$thisWrap.on('touchend',tEnd);
+function tStart(e) {
+	e.preventDefault();
+	var event = e.originalEvent;
+	first_point = event.touches[0].pageX;
+	console.log(first_point)
+}
+function tMove(e) {
+	e.preventDefault();
+	var event = e.originalEvent;
+	move_point = event.touches[0].pageX;
+	$thisBox.css("left",move_point);
+	console.log(move_point);
+}
+function tEnd(e) {
+	e.preventDefault();
+	$thisBox.css("left",move_point);
+	console.log("Fin");
+}
